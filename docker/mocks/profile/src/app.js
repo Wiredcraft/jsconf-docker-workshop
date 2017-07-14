2a7d7114f6e8ba6e947f531777db7e8791f2ca4b
Xavier Zhou <xiayezhou@gmail.com>
2017-07-14 23:16:39 +0800

    Change port number

--- docker/mocks/profile/src/app.js
+++ docker/mocks/profile/src/app.js
@@ -4,13 +4,14 @@ var express = require('express')
 var app = express()
 
 app.get('/profiles/:userName', function (req, res) {
+  // Bob is a bad guy 
   if (req.params.userName === 'bob') {
     return res.json({ name: 'bob', isGood: false})
   }
-  return res.json({ name: req.param.userName, isGood: true })
+  return res.json({ name: req.params.userName, isGood: true })
 })
 
-app.listen(3003, () => {
-  console.log('Mock Profile listening on port 3003');
+app.listen(3007, () => {
+  console.log('Mock Profile listening on port 3007');
 })
 
