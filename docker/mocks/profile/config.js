b481b0e6847fa8dfe55fcf15d0db5f3c6202bb98
Xavier Zhou <xiayezhou@gmail.com>
2017-07-14 23:37:01 +0800

    Use apib to mock API

---
+++ docker/mocks/profile/config.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  sourceFiles: __dirname + '/index.apib',
+  serverPort: 3007,
+  stealthmode: false,
+  disableCORS: false,
+  public: true,
+  autoOptions: true,
+  discover: false,
+  watch: true
+};
+
