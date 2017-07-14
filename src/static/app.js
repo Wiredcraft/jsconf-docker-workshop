f3afbfde4034b45e816bc6314ea4f656922791ad
wwayne <wayne.wang0821@gmail.com>
2017-07-14 22:46:39 +0800

    Update frontend js post

--- src/static/app.js
+++ src/static/app.js
@@ -10,6 +10,8 @@ $(document).ready(() => {
     $.ajax({
       url: '/people/count',
       method: 'POST',
+      dataType: 'json',
+      data: { name: 'oliver' },
       success: (res) => {
         $('#count').text(res.count)
       }
